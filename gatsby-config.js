97aaf471687ce6c20e20aa0761452c6bb19df087
Vsevolod <vsevaseva@yandex.ru>
2020-06-04 22:02:44 +0300

    Updated packages, removed unused

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,13 +33,6 @@ module.exports = {
               maxWidth: 590,
             },
           },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
         ],
@@ -48,17 +41,10 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    'gatsby-plugin-feed',
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Gatsby Starter Blog',
-        short_name: 'GatsbyJS',
+        name: 'Gatsby Pizza Delivery',
+        short_name: 'PizzaPizza',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#663399',

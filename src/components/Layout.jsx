aa2b8646e6ffe49e2ac52f2730642777d1d9369b
Vsevolod <vsevaseva@yandex.ru>
2020-06-04 22:29:50 +0300

    Installed use-cart, refactored layout to functional component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,69 +2,66 @@ import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props;
-    const rootPath = '/';
-    let header;
+const Layout = ({ location, title, children }) => {
+  const rootPath = '/';
+  let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1 style={{ marginTop: 0 }}>
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3
+  if (location.pathname === rootPath) {
+    header = (
+      <h1 style={{ marginTop: 0 }}>
+        <Link
           style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
           }}
+          to="/"
         >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h3>
-      );
-    }
-    return (
-      <div
+          {title}
+        </Link>
+      </h1>
+    );
+  } else {
+    header = (
+      <h3
         style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
+          fontFamily: 'Montserrat, sans-serif',
+          marginTop: 0,
         }}
       >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          ©
-          {' '}
-          {new Date().getFullYear()}
-          , Built with
-          {' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        <Link
+          style={{
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          to="/"
+        >
+          {title}
+        </Link>
+      </h3>
     );
   }
-}
+  return (
+    <div
+      style={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        ©
+        {' '}
+        {new Date().getFullYear()}
+        , Built with
+        {' '}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  );
+};
 
 Layout.propTypes = {
   location: PropTypes.any.isRequired,

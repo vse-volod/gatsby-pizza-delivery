9d25d5d381ba98cd899cad9b45bf28e567d9816b
Vsevolod <vsevaseva@yandex.ru>
2020-06-10 01:06:18 +0300

    Added success and empty messages for form

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
 import tw from 'twin.macro';
 import styled from '@emotion/styled';
+import { useCart } from '../utils/useCart';
+
+const Heading = styled.h1`
+  ${tw`text-3xl font-bold px-3 pt-4`}
+  color: #D45D27;
+`;
 
 const Input = styled.input`
   border-radius: 1rem;
@@ -26,28 +32,42 @@ const OrderFormButton = styled.input`
 
 const OrderForm = ({ onSubmit }) => {
   const { register, handleSubmit, errors } = useForm();
-  console.log('errors:', errors);
-
+  const [displayForm, toggleDisplayForm] = useState(true);
+  const { itemsCount, clearCart } = useCart();
+  const submitForm = (data) => {
+    toggleDisplayForm(false);
+    clearCart();
+    onSubmit(data);
+  };
+  if (!displayForm) {
+    return <Heading>Thank you for the order!</Heading>;
+  }
+  if (itemsCount === 0) {
+    return <div>Add some pizzas to your cart first!</div>;
+  }
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <FieldLabel>Your first name:</FieldLabel>
-      <Input type="text" error={errors && errors['First name']} name="First name" ref={register({ required: { value: true, message: 'this field required' } })} />
-      <FieldLabel>Your last name:</FieldLabel>
-      <Input type="text" error={errors && errors['Last name']} name="Last name" ref={register({ required: { value: true, message: 'this field required' } })} />
-      <FieldLabel>Delivery address:</FieldLabel>
-      <Input type="text" error={errors && errors.Address} name="Address" ref={register({ required: { value: true, message: 'this field required' } })} />
-      <FieldLabel>Mobile number:</FieldLabel>
-      <Input
-        type="tel"
-        error={errors && errors['Mobile number']}
-        name="Mobile number"
-        ref={register({
-          required: { value: true, message: 'this field required' },
-        })}
-      />
+    <>
+      <Heading>Complete your order:</Heading>
+      <Form onSubmit={handleSubmit(submitForm)}>
+        <FieldLabel>Your first name:</FieldLabel>
+        <Input type="text" error={errors && errors['First name']} name="First name" ref={register({ required: { value: true, message: 'this field required' } })} />
+        <FieldLabel>Your last name:</FieldLabel>
+        <Input type="text" error={errors && errors['Last name']} name="Last name" ref={register({ required: { value: true, message: 'this field required' } })} />
+        <FieldLabel>Delivery address:</FieldLabel>
+        <Input type="text" error={errors && errors.Address} name="Address" ref={register({ required: { value: true, message: 'this field required' } })} />
+        <FieldLabel>Mobile number:</FieldLabel>
+        <Input
+          type="tel"
+          error={errors && errors['Mobile number']}
+          name="Mobile number"
+          ref={register({
+            required: { value: true, message: 'this field required' },
+          })}
+        />
 
-      <OrderFormButton type="submit" />
-    </Form>
+        <OrderFormButton type="submit" />
+      </Form>
+    </>
   );
 };
 
--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import tw from 'twin.macro';
-import styled from '@emotion/styled';
 import Layout from '../components/Layout';
 import Cart from '../components/Cart';
 import OrderForm from '../components/OrderForm';
 import useExchangeRate from '../utils/useExchangeRate';
 import SEO from '../components/Seo';
 
-const Heading = styled.h1`
-  ${tw`text-3xl font-bold px-3 pt-4`}
-  color: #D45D27;
-`;
 
 const OrderPage = () => {
   const exchangeRate = useExchangeRate('EUR_USD');
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Layout>
       <SEO title="Order" />
-      <Heading id="order-review">Your order:</Heading>
-      <Cart exchangeRate={exchangeRate} hideFooter />
-      <Heading>Complete your order:</Heading>
-      <OrderForm onSubmit={onSubmit} />
+      <div id="order-review">
+        <Cart id="order-review" exchangeRate={exchangeRate} hideFooter />
+        <OrderForm onSubmit={onSubmit} />
+      </div>
     </Layout>
   );
 };

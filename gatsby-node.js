face9e5ca9edc704a3d43b513ef7b1fbe46a72c2
Vsevolod <vsevaseva@yandex.ru>
2020-06-10 02:28:00 +0300

    Fixes and cleanup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,6 @@
-const path = require('path');
+// const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-
-  const pizzaTemplate = path.resolve('./src/templates/Pizza.jsx');
-  const result = await graphql(
-    `
-      {
-        allMdx(
-          sort: { fields: [frontmatter___title], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `,
-  );
-
-  if (result.errors) {
-    throw result.errors;
-  }
-
-  // Create pizza pages.
-  const pizzas = result.data.allMdx.edges;
-
-  pizzas.forEach((pizza) => {
-    createPage({
-      path: pizza.node.fields.slug,
-      component: pizzaTemplate,
-      context: {
-        slug: pizza.node.fields.slug,
-      },
-    });
-  });
-};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;

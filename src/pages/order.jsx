be86110864b41d79d3edfdb7ccabca9fde39abdb
Vsevolod <vsevaseva@yandex.ru>
2020-06-10 20:10:06 +0300

    Moved order page functionality to separate component, added cleaning cart on order success

--- src/pages/order.jsx
+++ src/pages/order.jsx
@@ -1,46 +1,12 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Layout from '../components/Layout';
-import Cart from '../components/Cart';
-import OrderForm from '../components/OrderForm';
-import Heading from '../components/Heading';
-import useExchangeRate from '../utils/useExchangeRate';
 import SEO from '../components/Seo';
+import OrderHandler from '../components/OrderHandler';
 
-const OrderPage = () => {
-  const [showErrorMessage, toggleErrorMessage] = useState(false);
-  const [showSuccessMessage, toggleSuccessMessage] = useState(false);
-  const exchangeRate = useExchangeRate('EUR_USD');
-  const formUrl = `https://getform.io/f/${process.env.GETFORM_API_KEY}`;
-  const onSubmit = (data) => {
-    axios.post(formUrl, data)
-      .then((response) => {
-        if (response.status === 200) {
-          toggleErrorMessage(false);
-          toggleSuccessMessage(true);
-          return;
-        }
-        toggleErrorMessage(true);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  return (
-    <Layout>
-      <SEO title="Order" />
-      {showSuccessMessage ? (
-        <Heading>Thank you for the order!</Heading>
-      ) : (
-        <div id="order-review">
-          <Cart id="order-review" exchangeRate={exchangeRate} hideFooter />
-          <OrderForm onSubmit={onSubmit} />
-          {showErrorMessage
-          && <div>An error occured while sending your order, please try again</div>}
-        </div>
-      )}
-    </Layout>
-  );
-};
+const OrderPage = () => (
+  <Layout>
+    <SEO title="Order" />
+    <OrderHandler />
+  </Layout>
+);
 export default OrderPage;
